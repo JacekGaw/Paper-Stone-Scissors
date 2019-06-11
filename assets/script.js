ee1878df26a24a06ed1dde0844b29a7aa6eeee1c
Jacek <jacek@webdev.com>
2019-06-11 02:38:06 +0200

    Updated layout of Game Plate

--- assets/script.js
+++ assets/script.js
@@ -2,19 +2,26 @@ const name = document.querySelector('#name');
 const pointsToWin = document.querySelector('#points');
 const startButton = document.querySelector('.form__input--button');
 let formOutput = document.querySelector('.form__output');
-startButton.addEventListener('click',function()
-{
+const gamePanel = document.querySelector('#game-panel');
+
+startButton.addEventListener('click',validateForm);
+
+function validateForm(){
     if(name.value=="" || pointsToWin.value==""){
         formOutput.innerHTML = "You must insert informations to all inputs";
     }
     else {
         formOutput.innerHTML = "";
-
         for(let i=0; i<=pointsToWin.value.length; i++){
             if(isNaN(pointsToWin.value.charAt(i))){
                 formOutput.innerHTML = "Points to win is not a number";
             }
         }
+        openGamePanel();
     }
     
-});
+};
+
+function openGamePanel(){
+    
+};
